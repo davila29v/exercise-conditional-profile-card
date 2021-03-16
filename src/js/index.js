6db323803093f73f7a75c6ac960742c35ff6a017
davila29v <davila29v@hotmail.com>
2021-03-16 21:06:12 +0000

    cambios menores, re test, funciona bien

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,6 @@ import "../style/index.scss";
         github: "alesanchezr",
         linkedin: null,
         instagram: null,
-
         name: null,
         lastname: null,
         role: null,
@@ -32,15 +31,15 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>"${variables.name}"</h1>
-          <h2>"${variables.role}"</h2>
-          <h3>"${variables.city}", ""${variables.country}"</h3>
-          <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter}"<i class="fa fa-twitter"></i></a></li>
+          <img src=${variables.avatarURL} class="photo" />
+          <h1>${variables.name}, ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://www.instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/${variables.linkedin}"/><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${variables.instagram}"/><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -60,15 +59,15 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "#",
+    github: "#",
+    linkedin: "#",
+    instagram: "#",
+    name: "",
+    lastname: "",
+    role: "",
+    country: "",
+    city: ""
   };
   render(window.variables); //render the card for the first time
 
